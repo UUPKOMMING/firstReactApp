4327555265f8828c70c896bf7c0f41a2f5095bb0
UUPKOMMING <opatafrancis6@gmail.com>
2020-07-02 14:30:08 +0100

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Customheader = () => {
+  return (
+    <header className="header">
+      <div className="header__logo-box">
+        <img src={logo} className="header__App-logo" alt="logo" />
+      </div>
+      <div className="header__text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
+        </h1>
+        <a href='#' className='btn btn--white btn--animated'>Discover our tours</a>
+      </div>
+    </header>
+  );
+};
+
+export default Customheader;
---
+++ src/components/sec1.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Sec1 = () => {
+  return (
+    <main>
+      <section className="section-about">
+        <div className="u-center-text u-marginbottom-8">
+          <h2 className="heading-secondary">
+            Exiting Tours For Avdventurous people
+          </h2>
+        </div>
+        <div className="parent">
+          <div className="child-row-1-2">
+            <h3 className="heading-tertiary u-marginbottom-2 ">
+            
+              you're gonna fall in love
+            </h3>
+            <p className="paragraph u-marginbottom-2 ">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+              vel omnis provident. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Dolorem reiciendis enim ea mollitia cupiditate,
+              velit perspiciatis esse illo quia doloremque dicta atque dolores
+              accusamus dolore rerum inventore labore similique expedita!
+            </p>
+            <h3 className="heading-tertiary u-marginbottom-2">
+             
+              Live adventures you have never had before
+            </h3>
+            <p className="paragraph  u-marginbottom-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+              vel omnis provident. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Dolorem reiciendis enim ea mollitia cupiditate,
+              velit perspiciatis esse illo quia doloremque dicta atque dolores
+              accusamus dolore rerum inventore labore similique expedita!
+            </p>
+            <a href='#' className='btn-txt'>Learn More &rarr</a>
+          </div>
+          <div className="child-row-1-2">image</div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Sec1;
